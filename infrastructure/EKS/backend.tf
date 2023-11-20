57e7acf1cc1e0807bc63f1817566ceda37e21ab9
GowthamS-7 <92256560+GowthamS-7@users.noreply.github.com>
2023-11-20 13:33:05 +0530

    Update backend.tf

--- infrastructure/EKS/backend.tf
+++ infrastructure/EKS/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "ror-eks-test"
     key            = "terraform-eks/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "ror-eks-cluster"
+    dynamodb_table = "ror-eks-db"
   }
 }
